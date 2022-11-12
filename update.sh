2679f14dba463119f3aed013b7c84d1b3684a3c5
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-11-12 17:32:41 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -23,6 +23,12 @@ if [[ "$hst" != "$dart" ]]; then
 echo "$localip $(hostname)" >> /etc/hosts
 fi
 
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "$green      Membuat Folder Server        $NC"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+sleep 2
+clear
+
 mkdir -p /etc/xray
 mkdir -p /etc/v2ray
 touch /etc/xray/domain
@@ -54,12 +60,23 @@ rm tools.sh
 sleep 2
 clear
 
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "$green      Package tambahan VPS           $NC"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+sleep 2
+clear
+
 #command connect vps
 wget -O /usr/local/bin/add-ssh-user "https://raw.githubusercontent.com/hidessh99/seller-pub/main/add-ssh-user.sh" && chmod +x /usr/local/bin/add-ssh-user
 wget -O /usr/local/bin/add-vmess-user "https://raw.githubusercontent.com/hidessh99/seller-pub/main/add-vmess-user.sh" && chmod +x /usr/local/bin/add-vmess-user
 wget -O /usr/local/bin/del-vmess-user "https://raw.githubusercontent.com/hidessh99/seller-pub/main//del-vmess-user.sh" && chmod +x /usr/local/bin/del-vmess-user
 sleep 2
 
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "$green      Hapus Log Server           $NC"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+sleep 2
+clear
 
 #remove log
 wget -q -O /usr/bin/removelog "https://raw.githubusercontent.com/hidessh99/HIDE-package/main/log.sh" && chmod +x /usr/bin/removelog
