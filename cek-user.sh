64bcd5f86e0f4fe213061181797c6ae9b7a83b0a
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-11-12 20:14:01 +0700

    Create cek-user.sh

---
+++ cek-user.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /usr/bin/
+wget -O cek-ws "https://raw.githubusercontent.com/bracoli/v4/main/xray/cek-ws.sh" && chmod +x cek-ws
+wget -O cek-vless "https://raw.githubusercontent.com/bracoli/v4/main/xray/cek-vless.sh" && chmod +x cek-vless
+wget -O cek-tr "https://raw.githubusercontent.com/bracoli/v4/main/xray/cek-tr.sh" && chmod +x cek-tr
