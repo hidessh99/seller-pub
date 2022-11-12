aca906a328b99aa81229315054b59b69e179860e
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-11-12 16:26:38 +0700

    Create del-vmess-user.sh

---
+++ del-vmess-user.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+read -p "User: " -e user
+exp=$(grep -wE "^### $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
+systemctl restart xray > /dev/null 2>&1
