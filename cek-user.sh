2d2ccaf7cd30f17721298c4ecda95190aed47104
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-11-13 10:48:42 +0700

    Update cek-user.sh

--- cek-user.sh
+++ cek-user.sh
@@ -4,3 +4,9 @@ cd /usr/bin/
 wget -O cek-ws "https://raw.githubusercontent.com/bracoli/v4/main/xray/cek-ws.sh" && chmod +x cek-ws
 wget -O cek-vless "https://raw.githubusercontent.com/bracoli/v4/main/xray/cek-vless.sh" && chmod +x cek-vless
 wget -O cek-tr "https://raw.githubusercontent.com/bracoli/v4/main/xray/cek-tr.sh" && chmod +x cek-tr
+wget -O userdelexpired "https://gitlab.com/hidessh/baru/-/raw/main/userdelexpired.sh" && chmod +x userdelexpired.sh
+
+
+cd
+echo "0 17 * * * root clear-log" >> /etc/crontab
+echo "50 * * * * root userdelexpired" >> /etc/crontab
